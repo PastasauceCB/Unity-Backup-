4ad686fd7d2b1ec3947ab19aa3296f9f92618b7f
Carter Brogdon <Carterbrogdon01@gmail.com>
2018-06-21 11:50:30 -0400

    6/21/18

---
+++ Im Bored/Assets/HUD_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Manager : MonoBehaviour {
+
+    int score;
+    public Text scoretext;
+
+    public Text scorerext;
+    // Use this for initialization
+    void Start() {
+        score = 0;
+    scoretext.text = "Score: " + score;
+    }
+	
+    public void UpdateScore(int amount) {
+
+        score = score + amount;
+        scoretext.text = "Score: " + score;
+    }
+
+}
